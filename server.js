62b5ecacd8bf640e80a7e961ab8cd505c3f466bf
liarahna <helia.rahnamaei@iths.se>
2025-01-20 11:48:51 +0100

    added connection to mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
-const app = app();
+const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Welcome to my mongoAPI");
 
-app.listen(PORT, () => {
-    console.log("listening to 3000");
+});
+app.post("/", (req, res) =>{
+    res.send("This is a post request");
+});
+ 
 
+mongoose.
+connect("mongodb+srv://lia:admin@heliasapi.nfp20.mongodb.net/Products-API?retryWrites=true&w=majority&appName=HeliasAPI")
+.then(() => {
+    console.log("connected to mongo");
+    app. listen(PORT, () => {
+        console.log("listening to 3000");
+        });
 });
