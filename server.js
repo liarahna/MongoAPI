a21e148bd1999641e059a79f9b3c31d34b40542c
liarahna <helia.rahnamaei@iths.se>
2025-01-20 15:07:51 +0100

    added more endpoints

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 3000;
+const Product = require("./models/productModel");
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to my mongoAPI");
@@ -10,13 +14,60 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) =>{
     res.send("This is a post request");
 });
- 
+
+app.put("/api/product/:id", async (req, res) => {
+
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    
+    if(!product){
+        return res.status(400).json({message: "Product does not exist"});
+    }
+
+    res.status(200).json(product)
+
+});
+
+app.get("/api/product/:id", async(req, res) => {
+    try {
+    
+        const{id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500);
+    }
+
+});
+
+app.get("/api/products", async(req, res) =>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+
+});
+
+app.post("/api/product", async (req, res) => {
+    
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(404);
+    }
+
+});
 
 mongoose.
-connect("mongodb+srv://lia:admin@heliasapi.nfp20.mongodb.net/Products-API?retryWrites=true&w=majority&appName=HeliasAPI")
+connect(process.env.MONGO_URI)
 .then(() => {
-    console.log("connected to mongo");
-    app. listen(PORT, () => {
-        console.log("listening to 3000");
-        });
+console.log("Connected to Mongo");
+app.listen(PORT, () => {
+    console.log("listening to 3000");
+}); 
 });
